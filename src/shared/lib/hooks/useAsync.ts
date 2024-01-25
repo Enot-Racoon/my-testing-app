a3452473653d5f7a59ef24032aad15c9a6a6bf05
Alexey Karasev <enotracoon2020@gmail.com>
2024-01-26 06:10:47 +0700

    Refine loader

--- src/shared/lib/hooks/useAsync.ts
+++ src/shared/lib/hooks/useAsync.ts
@@ -1,6 +1,7 @@
 import { useCallback, useEffect, useState } from 'react'
 
 export const useAsync = <T>(asyncFn: () => Promise<T>, immediate = true) => {
+  const [state, setState] = useState<'init' | 'pending' | 'resolved' | 'rejected'>('init')
   const [result, setResult] = useState<T | null>(null)
   const [pending, setPending] = useState<boolean>(false)
   const [error, setError] = useState<Error | null>(null)
@@ -8,14 +9,17 @@ export const useAsync = <T>(asyncFn: () => Promise<T>, immediate = true) => {
   const dispatch = useCallback(async () => {
     setError(null)
     setPending(true)
+    setState('pending')
 
     try {
       setResult(await asyncFn())
       setPending(false)
+      setState('resolved')
     } catch (e: unknown) {
       if (e instanceof Error) {
         setError(e)
         setPending(false)
+        setState('rejected')
       }
     }
   }, [asyncFn])
@@ -24,5 +28,5 @@ export const useAsync = <T>(asyncFn: () => Promise<T>, immediate = true) => {
     if (immediate) void dispatch()
   }, [asyncFn, dispatch, immediate])
 
-  return { result, pending, error, dispatch }
+  return { state, result, pending, error, dispatch }
 }
