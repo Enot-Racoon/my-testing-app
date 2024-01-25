734eee68e3516ce7313f9c4791dfdacf878becc5
Alexey Karasev <enotracoon2020@gmail.com>
2024-01-25 11:24:29 +0700

    Configured tests

---
+++ jest.config.ts
@@ -0,0 +1,12 @@
+export default {
+  preset: 'ts-jest',
+  testEnvironment: 'jest-environment-jsdom',
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
+  moduleNameMapper: {
+    '\\.(gif|ttf|eot|svg|png)$': '<rootDir>/jest/__mocks__/fileMock.js',
+    '\\.(css|scss)$': 'identity-obj-proxy',
+  },
+  resetMocks: true,
+}
